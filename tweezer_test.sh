4e22748381c942c2dfa9055f2f44ec69ef1e119e
yizheng jiao <jyizheng@gmail.com>
2022-12-28 07:31:00 -0500

    change scripts

--- tweezer_test.sh
+++ tweezer_test.sh
@@ -7,24 +7,26 @@ rm /mnt/splinterdb_cache.log
 
 rm -rf /mnt/rocksdb.db/
 
-iostat -p /dev/nvme0n1
+io_start=`iostat -p  | grep nvme`
 
 #/home/betrfs/linux-5.9.15/tools/perf/perf record -ag -o perf.log  -- \
 
+ls -lash /app/untrusted_memory 
+
 start=$(date +%s%N)
 
-
 #gdb --args \
-SCONE_VERSION=1 SCONE_HEAP=94371840 /home/betrfs/tweezer/Docker/ycsb-c/ycsbc -db rocksdb \
+SCONE_VERSION=1 SCONE_HEAP=98566144 /home/betrfs/tweezer/Docker/ycsb-c/ycsbc -db rocksdb \
         -threads 1 \
         -L workloads/load.spec -w fieldlength 1024 -w fieldcount 1 -w recordcount 2000000 \
-        -W workloads/workloada.spec -w operationcount 1000
+        -W workloads/workloada.spec -w operationcount 1000000
 
 end=$(date +%s%N)
 
 echo "Total Time is (ns):"
 echo "print $end - $start" | python
 
-iostat -p /dev/nvme0n1
-
-
+iostat -p  | grep Device
+io_end=`iostat -p  | grep nvme`
+echo ${io_start}
+echo ${io_end}
